a870814d970011d1f1a6ba700fd4786b5d249197
Robert <poparobert22@stud.ase.ro>
2024-11-18 21:23:40 +0200

    capturing mouse and keyboard

---
+++ capturing/keyboard.py
@@ -0,0 +1,22 @@
+from pynput import keyboard
+import logging
+
+keystrokes = ""
+
+def on_press(key):
+    global keystrokes
+    try:
+        if key == keyboard.Key.enter:
+            keystrokes += "\n"
+        elif key == keyboard.Key.tab:
+            keystrokes += "\t"
+        elif key == keyboard.Key.space:
+            keystrokes += " "
+        else:
+            keystrokes += str(key).strip("'")
+    except Exception:
+        logging.error("Error in on_press")
+            
+keyboardListener = keyboard.Listener(
+    on_press=on_press)
+keyboardListener.start()
